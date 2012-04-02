3039e9825ff64ff2109c46756ce3cad07cb67055
finscn <fins@163.com>
2012-04-02 15:03:38 +0800

    update doubleTap

--- src/custom/DoubleTap.js
+++ src/custom/DoubleTap.js
@@ -2,7 +2,7 @@
 	Toucher.DoubleTap=Toucher.Tap.extend({
 
 		delay : 800 ,
-		limit : 5,
+		limit : 10,
 
 		prevTap : null ,
 
@@ -20,13 +20,26 @@
 
 					// 有前一次tap 且 前一次tap的结束时间&点击位置在允许的范围内
 					if (this.prevTap){
-
+						
 						if (startTime-this.prevTap.endTime<=this.delay
 							&& Math.abs(pageX-this.prevTap.pageX)<=this.limit
 							&& Math.abs(pageY-this.prevTap.pageY)<=this.limit ){
 							this.onTap(wrapperList,event,touchController);
+							this.prevTap=null;
+							this.enabled=false;
+							return;
+						}else{
+
+							console.log([startTime-this.prevTap.endTime,
+										Math.abs(pageX-this.prevTap.pageX),
+										Math.abs(pageY-this.prevTap.pageY)
+											])
+
+							this.prevTap.endTime=touchWrapper.endTime;
+							this.prevTap.startTime=startTime;
+							this.prevTap.pageX=pageX;
+							this.prevTap.pageY=pageY;
 						}
-						this.prevTap=null;
 
 					}else{
 						// 没有前一次tap的话, 则创建,并记录此次tap的相关信息
@@ -39,6 +52,8 @@
 				}else{
 					this.prevTap=null;
 				}	
+			}else{
+				this.prevTap=null;
 			}
 
 			this.enabled=false;
